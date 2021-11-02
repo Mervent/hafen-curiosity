0a3654c4ec995ee59c319160033a6e61cca9b802
Mervent <22627512+Mervent@users.noreply.github.com>
2021-11-03 00:31:42 +0300

    Add basic automation

---
+++ src/me/mervent/automation/BotAPI.java
@@ -0,0 +1,128 @@
+package me.mervent.automation;
+
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import haven.*;
+
+public class BotAPI {
+	private GameUI gui;
+	public BotAPI(GameUI gui) {
+		this.gui = gui;
+	}
+	
+	// GENERAL
+	public WItem getItemAtHand() {
+		return gui.vhand;
+	}
+	
+        // INVENTORY
+	public  List<WItem> getInventoryItems(Inventory inventory) {
+		return inventory.children(WItem.class)
+                .stream()
+                .collect(Collectors.toList());
+	}
+
+	public List<WItem> getInventoryItemsByName(Inventory inventory, String pattern) {
+		Pattern pat = Pattern.compile(pattern);
+		return inventory.children(WItem.class).stream()
+				.filter(item -> {
+					String name = getItemName(item);
+					return (name != null && pat.matcher(name).matches());
+				})
+				.collect(Collectors.toList());
+	}
+
+	public List<WItem> getInventoryItemsByResName(Inventory inventory, String pattern) {
+		Pattern pat = Pattern.compile(pattern);
+		return inventory.children(WItem.class).stream()
+				.filter(item -> {
+					String resname = getItemResName(item);
+					return (resname != null && pat.matcher(resname).matches());
+				})
+				.collect(Collectors.toList());
+	}
+	public WItem getInventoryItemByLocation(Inventory inventory, int x, int y) {
+		for(WItem witem : inventory.children(WItem.class)) {
+			if(witem.c.div(UI.scale(33)).x == x && witem.c.div(UI.scale(33)).y == y) {
+				return witem;
+			}
+		}
+		return null;
+	}
+        public String getItemName(WItem item) {
+		while(true) {
+			try {
+				synchronized(item.ui) {
+					for(Object o : item.info().toArray()) {
+						if(o instanceof ItemInfo.Name)
+							return ((ItemInfo.Name) o).str.text;
+					}
+					break;
+				}
+			} catch(Loading l) { }
+			sleep(20);
+		}
+		return null;
+	}
+        
+        public String getItemResName(WItem item) {
+		while(true) {
+			try {
+				Resource res = item.item.getres();
+				if(res == null)
+					return null;
+				else
+					return res.name;
+			} catch(Loading l) {
+			}
+		}
+	}
+	public Coord getItemLocation(WItem item) {
+		return item.c.div(33);
+	}	
+	public void drop(Inventory inventory, int x, int y) {
+		inventory.wdgmsg("drop", new Coord(x, y));
+	}
+	public Coord getSize(Inventory inventory) {
+		return inventory.isz;
+	}
+	public void takeItem(WItem item, boolean wait) {
+		item.take();
+		if (wait) {
+			while(getItemAtHand() == null) {
+				sleep(25);
+			}
+		}
+	}
+	public Coord getSize(WItem item) {
+		try {
+			Indir<Resource> res = item.item.getres().indir();
+			if(res.get() != null && res.get().layer(Resource.imgc) != null) {
+				Tex tex = res.get().layer(Resource.imgc).tex();
+				if(tex == null)
+					return new Coord(1, 1);
+				else
+					return UI.unscale(tex.sz()).div(30);
+			} else {
+				return new Coord(1, 1);
+			}
+		} catch(Loading l) {
+
+		}
+		return new Coord(1, 1);
+	}
+	
+	// UTILITY
+	public void msg(String msg) {
+		gui.msg(msg);
+	}
+        public void sleep(int time) {
+		try {
+			Thread.sleep(time);
+		} catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}   
+}
---
+++ src/me/mervent/automation/InventorySort.java
@@ -0,0 +1,81 @@
+package me.mervent.automation;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+import haven.*;
+
+public class InventorySort {
+        private GameUI gui;
+        private BotAPI api;
+        private Pattern pattern = Pattern.compile("Cupboard|Chest|Crate|Woodbox|Basket");
+        private Comparator<WItem> ITEM_COMPARATOR = new Comparator<WItem>() {
+                @Override
+                public int compare(WItem o1, WItem o2) {
+                        if (api.getItemName(o1).compareTo(api.getItemName(o2)) == 0) {
+                                QualityList ql1 = o1.itemq.get();
+                                double q1 = (ql1 != null && !ql1.isEmpty()) ? ql1.single().value : 0;
+                        
+                                QualityList ql2 = o2.itemq.get();
+                                double q2 = (ql2 != null && !ql2.isEmpty()) ? ql2.single().value : 0;
+                        
+                                return Double.compare(q1, q2);
+                        }
+                        return api.getItemName(o1).compareTo(api.getItemName(o2));
+                    
+                }
+            };
+        
+        public InventorySort(GameUI gui) {
+                this.gui = gui;
+                this.api = new BotAPI(gui);
+        }
+        public void run() {
+                ArrayList<Inventory> inventories = new ArrayList<>();
+                
+		for(Window w : gui.children(Window.class)) {
+			if(pattern.matcher(w.cap.text).matches()) {
+                                for(Inventory i : w.children(Inventory.class)) {
+                                        inventories.add(i);
+                                }
+                                        
+                        }
+		}
+                
+                for(Inventory inv: inventories) {
+                        Boolean[][] grid = new Boolean[api.getSize(inv).x][api.getSize(inv).y];
+                        
+                        List<WItem> items = api.getInventoryItems(inv);
+                        Coord inventorySize = api.getSize(inv);
+                        items.sort(ITEM_COMPARATOR);
+                        int x = 0;
+                        int y = 0;
+                        api.msg(Integer.toString(inventorySize.x));
+                        api.msg(Integer.toString(inventorySize.y));
+                        for(WItem item: items) {
+                                
+                                // Coord location = api.getItemLocation(item);
+                                
+                                Coord prevLocation = api.getItemLocation(item);                                                                
+                                item.take();                                
+                                api.drop(inv, x, y);  
+                                api.msg(api.getItemName(item) + " place to " + Integer.toString(x) + "," + Integer.toString(y));  
+                                api.drop(inv, prevLocation.x, prevLocation.y);                                        
+                                api.msg(api.getItemName(item) + " back to " + Integer.toString(x) + "," + Integer.toString(y));  
+                                // if (api.getItemAtHand() != null) {
+                                //         api.msg(api.getItemName(api.getItemAtHand()) + " move to " + Integer.toString(x) + "," + Integer.toString(y));  
+                                //         api.drop(inv, prevLocation.x, prevLocation.y);                                        
+                                // }                            
+                                                                                                
+                                x += 1;
+                                if (x == inventorySize.x) {
+                                        x = 0;
+                                        y += 1;
+                                }
+                                                                                                                    
+                                
+                        }
+                }
+                
+        }
+}
